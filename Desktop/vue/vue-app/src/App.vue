878ad774ead31564c44a4431517368c93087a219
Quang Le <quang.lethanh@vti.com.vn>
2023-04-11 12:39:13 +0700

    first commit

---
+++ Desktop/vue/vue-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="background">
+    <CatFact />
+  </div>
+</template>
+
+<script>
+import CatFact from './components/CatFact.vue';
+export default {
+  name: 'App',
+  components: {
+    CatFact
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Roboto', Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+.background {
+  background-image: url("./assets/cat_image.jpg");
+  height: 100vh;
+  background-position:center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
---
+++ Desktop/vue/vue-app/src/components/CatFact.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <h1 class="title">
+            Daily Cat Fact <figcaption></figcaption>
+        </h1>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="fact-content"> {{ catFact }}</h2>
+                <button class="btn btn-primary" @click="getCatFact">More Facts</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "CatFact",
+        data() {
+            return {
+                catFact: ''
+            }
+        },
+        methods: {
+            async getCatFact() {
+                const response = await fetch("https://cat-fact.herokuapp.com/facts/random")
+                const jsonData = await response.json()
+                this.catFact = jsonData.text
+            }
+        },
+        mounted() {
+            this.getCatFact()
+        }
+    }
+</script>
+<style scoped>
+    .container {
+        padding-top: 10px;
+    }
+    .title {
+        margin-top: 100px;
+        color: azure;
+    }
+    .fact-content {
+        color: black
+    }
+    .card {
+        width: 800px;
+        margin: auto;
+    }
+    .card-body {
+        background-color:azure;
+    }
+</style>
